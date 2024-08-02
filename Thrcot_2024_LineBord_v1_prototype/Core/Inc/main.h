1ca7a6f45aeb934f69ce20a151e189f12b673d06
neo-Thrcot <neokishimoto@outlook.jp>
2024-08-03 06:06:37 +0900

    すべてのIOの割り当て設定

--- Thrcot_2024_LineBord_v1_prototype/Core/Inc/main.h
+++ Thrcot_2024_LineBord_v1_prototype/Core/Inc/main.h
@@ -57,8 +57,44 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LS8_Pin GPIO_PIN_0
+#define LS8_GPIO_Port GPIOA
+#define LS9_Pin GPIO_PIN_1
+#define LS9_GPIO_Port GPIOA
+#define LS10_Pin GPIO_PIN_2
+#define LS10_GPIO_Port GPIOA
+#define LS11_Pin GPIO_PIN_3
+#define LS11_GPIO_Port GPIOA
+#define LS12_Pin GPIO_PIN_4
+#define LS12_GPIO_Port GPIOA
+#define LS13_Pin GPIO_PIN_5
+#define LS13_GPIO_Port GPIOA
+#define LS14_Pin GPIO_PIN_6
+#define LS14_GPIO_Port GPIOA
+#define LS15_Pin GPIO_PIN_7
+#define LS15_GPIO_Port GPIOA
+#define LS1_Pin GPIO_PIN_0
+#define LS1_GPIO_Port GPIOB
+#define LS2_Pin GPIO_PIN_1
+#define LS2_GPIO_Port GPIOB
+#define LS16_Pin GPIO_PIN_8
+#define LS16_GPIO_Port GPIOA
+#define LS17_Pin GPIO_PIN_11
+#define LS17_GPIO_Port GPIOA
+#define LS18_Pin GPIO_PIN_12
+#define LS18_GPIO_Port GPIOA
 #define LD1_Pin GPIO_PIN_15
 #define LD1_GPIO_Port GPIOA
+#define LS3_Pin GPIO_PIN_3
+#define LS3_GPIO_Port GPIOB
+#define LS4_Pin GPIO_PIN_4
+#define LS4_GPIO_Port GPIOB
+#define LS5_Pin GPIO_PIN_5
+#define LS5_GPIO_Port GPIOB
+#define LS6_Pin GPIO_PIN_6
+#define LS6_GPIO_Port GPIOB
+#define LS7_Pin GPIO_PIN_7
+#define LS7_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
--- Thrcot_2024_LineBord_v1_prototype/Core/Src/main.c
+++ Thrcot_2024_LineBord_v1_prototype/Core/Src/main.c
@@ -40,6 +40,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
 
@@ -48,6 +49,7 @@
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -86,6 +88,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -97,10 +100,9 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	  HAL_GPIO_WritePin(LD1_GPIO_Port, LD1_Pin, 1);
-	  HAL_Delay(1000);
-	  HAL_GPIO_WritePin(LD1_GPIO_Port, LD1_Pin, 0);
-	  HAL_Delay(1000);
+	  int state = HAL_GPIO_ReadPin(LS1_GPIO_Port, LS1_Pin);
+
+	  HAL_GPIO_WritePin(LD1_GPIO_Port, LD1_Pin, state);
   }
   /* USER CODE END 3 */
 }
@@ -113,6 +115,7 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
@@ -141,6 +144,47 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1;
+  PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK1;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
 }
 
 /**
@@ -156,9 +200,28 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(LD1_GPIO_Port, LD1_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : LS8_Pin LS9_Pin LS10_Pin LS11_Pin
+                           LS12_Pin LS13_Pin LS14_Pin LS15_Pin
+                           LS16_Pin LS17_Pin LS18_Pin */
+  GPIO_InitStruct.Pin = LS8_Pin|LS9_Pin|LS10_Pin|LS11_Pin
+                          |LS12_Pin|LS13_Pin|LS14_Pin|LS15_Pin
+                          |LS16_Pin|LS17_Pin|LS18_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LS1_Pin LS2_Pin LS3_Pin LS4_Pin
+                           LS5_Pin LS6_Pin LS7_Pin */
+  GPIO_InitStruct.Pin = LS1_Pin|LS2_Pin|LS3_Pin|LS4_Pin
+                          |LS5_Pin|LS6_Pin|LS7_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /*Configure GPIO pin : LD1_Pin */
   GPIO_InitStruct.Pin = LD1_Pin;
